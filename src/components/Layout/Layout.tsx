c643f8d08aa1f9e497f9e2d47d7e0d1b8e56463f
ilyacnt <ilyagt2012@gmail.com>
2023-08-14 11:32:46 +0300

    Pages base

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,12 @@
+import Header from '../Header/Header'
 import styles from './Layout.module.css'
 
 const Layout = () => {
-    return <div className={styles.Layout}>Layout</div>
+    return (
+        <div className={styles.Layout}>
+            <Header />
+        </div>
+    )
 }
 
 export default Layout
---
+++ src/pages/List/List.tsx
@@ -0,0 +1,7 @@
+import styles from './List.module.css'
+
+const List = () => {
+    return <div className={styles.List}>List</div>
+}
+
+export default List
